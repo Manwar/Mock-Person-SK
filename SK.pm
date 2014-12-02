4dd82f626be672af5f3811a812c314a6424e71fb
Mohammad Anwar <mohammad.anwar@ntt.eu>
2014-12-02 15:32:16 +0000

    - Fixed bug RT: 100664.

--- SK.pm
+++ SK.pm
@@ -210,7 +210,7 @@ Zlatko
 Zoltán
 );
 
-# First nad middle female names.
+# First and middle female names.
 our @first_female = our @middle_female = qw(
 Adela
 Adriána
@@ -1251,15 +1251,27 @@ Mock::Person::SK - Generate random sets of Slovak names.
 
 =head1 SYNOPSIS
 
- use Mock::Person::SK qw(first_male first_female last_male last_female
-         middle_male middle_female name);
- my $first_male = first_male();
- my $first_female = first_female();
- my $last_male = last_male();
- my $last_female = last_female();
- my $middle_male = middle_male();
- my $middle_female = middle_female();
- my $name = name($sex);
+use Encode qw(encode_utf8);
+use Mock::Person::SK qw(
+    first_male
+    first_female
+    last_male
+    last_female
+    middle_male
+    middle_female
+    name);
+
+my $first_male    = first_male();
+my $first_female  = first_female();
+my $last_male     = last_male();
+my $male_name     = name('male');
+print "Name (Male)  : ", encode_utf8($male_name)  , "\n";
+
+my $last_female   = last_female();
+my $middle_male   = middle_male();
+my $middle_female = middle_female();
+my $female_name   = name('female');
+print "Name (Female): ", encode_utf8($female_name), "\n";
 
 =head1 DESCRIPTION
 
